67b3a549ad285456185ed4412e344f896390c8fa
Andrey Sitnik <andrey@sitnik.ru>
2015-09-14 01:28:11 +0300

    Clean up code

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ module.exports = function (talk) {
                 if ( err ) return reject(err);
                 data.css += syntaxCss;
                 resolve(data);
-            })
+            });
         });
     });
 };
