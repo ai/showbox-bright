35ed54a563d46dedc3d86fa8d020448bfd8aac2c
Andrey Sitnik <andrey@sitnik.ru>
2015-09-14 01:13:17 +0300

    Add styles for syntax highlight

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var shower = require('showbox-shower');
 var path   = require('path');
+var fs     = require('fs');
 
 module.exports = function (talk) {
-    var npm     = path.join(__dirname, 'node_modules');
-    var cssFile = path.join(npm, '/shower-bright/styles/screen.css');
-    return shower(cssFile, talk);
+    var npm        = path.join(__dirname, 'node_modules');
+    var cssFile    = path.join(npm, '/shower-bright/styles/screen.css');
+    var syntaxFile = path.join(__dirname, 'syntax.css');
+
+    return shower(cssFile, talk).then(function (data) {
+        return new Promise(function (resolve, reject) {
+            fs.readFile(syntaxFile, function (err, syntaxCss) {
+                if ( err ) return reject(err);
+                data.css += syntaxCss;
+                resolve(data);
+            })
+        });
+    });
 };
